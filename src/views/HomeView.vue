35766948f45168f20a92b271817f3c333bcf296a
jiangrui <>
2023-11-06 15:07:39 +0800

    【web】【issue_id=xxx】数据和方法

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld :msg="msg"/>
+    <button @click="editMsgHandle">按钮</button>
   </div>
 </template>
 
@@ -11,8 +12,18 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    editMsgHandle() {
+      this.msg = 'new msg'
+    },
+  },
   components: {
     HelloWorld
-  }
+  },
 }
 </script>
