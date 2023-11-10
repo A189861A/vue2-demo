664ebd4e23de796dcb9a4a463e7e9a06e02a7998
jiangrui <>
2023-11-10 20:49:35 +0800

    【web】【issue_id=xxx】模拟从接口异步加载组件

--- src/views/AsyncView.vue
+++ src/views/AsyncView.vue
@@ -11,9 +11,19 @@ export default{
     return {}
   },
   components: {
-    // 遇到异步组件，会将其从主脚本中分离，减少脚本体积，加快首屏加载时间。当遇到场景需要使用该组件时，才会去加载组件脚本。
-    'tabArchive': () => import('../components/tabArchive.vue'),
     // tabArchive
+
+    // 遇到异步组件，会将其从主脚本中分离，减少脚本体积，加快首屏加载时间。当遇到场景需要使用该组件时，才会去加载组件脚本。
+
+    // 写法一
+    // 'tabArchive': () => import('../components/tabArchive.vue'),
+
+    // 写法二
+    'tabArchive': (resolve) => {
+      setTimeout(() => {
+        return require(['../components/tabArchive.vue'], resolve);
+      }, 2000);
+    },
   },
   mounted(){
   },
